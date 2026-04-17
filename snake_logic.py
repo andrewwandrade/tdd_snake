51053b8c7067ec590c57bf9b4aef47c5902597de
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-04-17 10:20:06 -0300

    :green_square: GREEN: implementa lógica básica de movimento

--- snake_logic.py
+++ snake_logic.py
@@ -3,3 +3,17 @@ class Snake:
         self.width = width
         self.height = height
         self.body = [(width//2, height//2)]
+    
+    def move(self, direction):
+        x, y = self.body[0]
+
+        if direction == 'd':
+            x+=1
+        elif direction == 'a':
+            x-=1
+        elif direction == 'w':
+            y+=1
+        elif direction == 's':
+            y-=1
+        
+        self.body = [(x, y)]
