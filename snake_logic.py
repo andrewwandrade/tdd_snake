7ce495624db7b28003d595b07fca3b4085efcc18
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-04-17 10:41:25 -0300

    :blue_square: REFACTOR: extrai criação de frutas para método spawn_fruit

--- snake_logic.py
+++ snake_logic.py
@@ -12,7 +12,16 @@ class Snake:
         self.width = width
         self.height = height
         self.body = [(width//2, height//2)]
-        self.fruits = [(random.randint(0, width-1), random.randint(0, height-1))]
+        self.fruits = []
+        self.spawn_fruit()
+
+    def spawn_fruit(self):
+        while True:
+            new_fruit = (random.randint(0, self.width - 1), 
+                         random.randint(0, self.height - 1))
+            if new_fruit not in self.body:
+                self.fruits.append(new_fruit)
+                break
 
     def _calculate_new_position(self, direction):
         dx, dy = MOVES[direction]
@@ -20,13 +29,15 @@ class Snake:
         return (x + dx) % self.width, (y + dy) % self.height
 
     def move(self, direction):
-        if direction in MOVES:
-            new_head = self._calculate_new_position(direction)
-            
-            self.body.insert(0, new_head)
-            
-            if new_head in self.fruits:
-                self.fruits.remove(new_head)
-                self.fruits.append((0,0)) 
-            else:
-                self.body.pop()
+        if direction not in MOVES:
+            return
+
+        new_head = self._calculate_new_position(direction)
+        
+        self.body.insert(0, new_head)
+
+        if new_head in self.fruits:
+            self.fruits.remove(new_head)
+            self.spawn_fruit()
+        else:
+            self.body.pop()
