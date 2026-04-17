2729dea876763633f93df246e387a8b9c238ff3f
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-04-17 10:37:54 -0300

    :green_square: GREEN: implementa lógica de crescimento e consumo de frutas

--- snake_logic.py
+++ snake_logic.py
@@ -1,3 +1,5 @@
+import random
+
 MOVES = {
     'w': (0, -1),
     's': (0, 1),
@@ -10,6 +12,7 @@ class Snake:
         self.width = width
         self.height = height
         self.body = [(width//2, height//2)]
+        self.fruits = [(random.randint(0, width-1), random.randint(0, height-1))]
 
     def _calculate_new_position(self, direction):
         dx, dy = MOVES[direction]
@@ -19,4 +22,11 @@ class Snake:
     def move(self, direction):
         if direction in MOVES:
             new_head = self._calculate_new_position(direction)
-            self.body = [new_head]
+            
+            self.body.insert(0, new_head)
+            
+            if new_head in self.fruits:
+                self.fruits.remove(new_head)
+                self.fruits.append((0,0)) 
+            else:
+                self.body.pop()
