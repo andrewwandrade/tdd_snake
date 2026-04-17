2b4e35ca076f6d3da5681a65d114503bb67870ce
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-04-17 10:21:33 -0300

    :blue_square: REFACTOR: otimiza movimentação usando coordenadas

--- snake_logic.py
+++ snake_logic.py
@@ -1,19 +1,17 @@
+MOVES = {
+    'w': (0, -1),
+    's': (0, 1),
+    'a': (-1, 0),
+    'd': (1, 0)
+}
+
 class Snake:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.body = [(width//2, height//2)]
-    
-    def move(self, direction):
-        x, y = self.body[0]
 
-        if direction == 'd':
-            x+=1
-        elif direction == 'a':
-            x-=1
-        elif direction == 'w':
-            y+=1
-        elif direction == 's':
-            y-=1
-        
-        self.body = [(x, y)]
+    def move(self, direction):
+        dx, dy = MOVES[direction]
+        x, y = self.body[0]
+        self.body = [(x + dx, y + dy)]
