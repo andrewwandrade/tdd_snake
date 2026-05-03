a5bc0027f420fbc8e3821f91aa9cb1dc4dcd6cbe
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-05-03 13:37:33 -0300

    :green_square: GREEN: insere função para mapear teclas

--- snake_pygame.py
+++ snake_pygame.py
@@ -2,6 +2,14 @@ import pygame
 
 CELL_SIZE = 32
 
+KEY_MAP = {
+    pygame.K_w: 'w',
+    pygame.K_a: 'a',
+    pygame.K_s: 's',
+    pygame.K_d: 'd',
+    pygame.K_ESCAPE: 'end',
+}
+
 class PygameRenderer:
     def __init__(self, width: int, height: int, cell_size: int = CELL_SIZE):
         self.width = width
@@ -17,6 +25,9 @@ class PygameRenderer:
         self.clock = pygame.time.Clock()
         self.font_big = pygame.font.SysFont("monospace", 36, bold=True)
         self.font_small = pygame.font.SysFont("monospace", 20)
+
+    def translate_key(self, key: int):
+        return KEY_MAP.get(key, None)
 
     def quit(self):
         pygame.quit()
