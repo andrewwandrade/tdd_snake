d7f037dcb636508d3573f51df425e9f8fd16b0d0
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-05-03 12:37:17 -0300

    :green_square: GREEN: pygamerenderer inicializa com dimensões corretas

---
+++ snake_pygame.py
@@ -0,0 +1,17 @@
+import pygame
+
+CELL_SIZE = 32
+
+class PygameRenderer:
+    def __init__(self, width: int, height: int, cell_size: int = CELL_SIZE):
+        self.width = width
+        self.height = height
+        self.cell_size = cell_size
+
+        pygame.init()
+        self.screen = pygame.display.set_mode(
+            (width * cell_size, height * cell_size)
+        )
+
+    def quit(self):
+        pygame.quit()
