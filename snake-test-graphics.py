f573a2be67f43350a4bcddbb2f7565966c40405e
Andrew de Andrade <andrewalmeida2110@gmail.com>
2026-05-03 13:43:41 -0300

    :red_square: RED: adiciona testes para renderização com build_frame

--- snake-test-graphics.py
+++ snake-test-graphics.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+from snake_logic import Snake
 
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 os.environ["SDL_AUDIODRIVER"] = "dummy"
@@ -30,4 +31,19 @@ def test_translate_unknown_key_returns_none():
     pygame.init()
     renderer = PygameRenderer(width=10, height=10, cell_size=20)
     assert renderer.translate_key(pygame.K_z) is None
+    renderer.quit()
+
+def test_build_frame_does_not_raise_exception():
+    pygame.init()
+    renderer = PygameRenderer(width=10, height=10, cell_size=20)
+    game = Snake(10, 10)
+    renderer.build_frame(game) 
+    renderer.quit()
+
+def test_build_frame_snake_with_multiple_segments():
+    pygame.init()
+    renderer = PygameRenderer(width=20, height=20, cell_size=20)
+    game = Snake(20, 20)
+    game.body = [(5, 5), (4, 5), (3, 5)]
+    renderer.build_frame(game)
     renderer.quit()
